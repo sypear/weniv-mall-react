9fd46f19a4c8cb3ee59be6eeb85d1fe186cb58b5
sypear <bsyeon015@gmail.com>
2022-11-28 19:33:59 +0900

    ✨ 메인 페이지 구현

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import { data } from '../../database/data';
+import Card from '../../components/Card/Card';
+import Cart from '../../components/Cart/Cart';
+import { Link, useLocation } from 'react-router-dom';
+import InfoModal from '../../components/Modal/Modal';
+
+const ProductList = styled.ul`
+  width: 1260px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 60px;
+`;
+
+export default function HomePage() {
+  const location = useLocation();
+
+  return (
+    <>
+      <ProductList>
+        {/* <Link to="/modal" state={{ background: location }}>
+          Open Modal
+        </Link> */}
+        {
+          data.map((item) => (
+            <Link key={item.id} to={`/${item.id}`} state={{ background: location }}>
+              <Card {...item} />
+            </Link>
+          ))
+        }
+      </ProductList>
+      <Cart />
+    </>
+  )
+}

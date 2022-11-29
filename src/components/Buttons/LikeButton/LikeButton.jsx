e53752a69a5199e7ad8664534fbe1d5d934b1cc8
sypear <bsyeon015@gmail.com>
2022-11-29 22:45:54 +0900

    💄 LikeButton 컴포넌트 background 등 스타일 추가

--- src/components/Buttons/LikeButton/LikeButton.jsx
+++ src/components/Buttons/LikeButton/LikeButton.jsx
@@ -9,10 +9,13 @@ const Button = styled.button`
   border: 1px solid var(--button-border-color);
   border-radius: 5px;
   background: url(${HeartOffIcon}) no-repeat center;
+  ${props => props.isSoldOut ? 'pointer-events: none;' : null};
+  overflow: hidden;
+  text-indent: -99999px;
 `;
 
-export default function LikeButton() {
+export default function LikeButton({isSoldOut}) {
   return (
-    <Button />
+    <Button isSoldOut={isSoldOut}>좋아요</Button>
   )
 }

97900a9d7ac8a17848936c63c25a57c328ce59ea
sypear <bsyeon015@gmail.com>
2022-11-29 22:45:02 +0900

    ✨ FillButton 컴포넌트 background-color, pointer-event 스타일 추가

--- src/components/Buttons/FillButton/FillButton.jsx
+++ src/components/Buttons/FillButton/FillButton.jsx
@@ -4,19 +4,20 @@ import styled from 'styled-components'
 const Button = styled.button`
   ${props => props.pageType === 'page' ? 'flex: 1;' : null}
   ${props => props.pageType !== 'page' ? 'width: 308px;' : null}
-  padding: 19px 119px;
   border-radius: 5px;
   text-align: center;
   font-size: 18px;
   font-weight: 700;
   line-height: 22px;
   color: var(--white);
-  background-color: var(--main-color);
+  background-color: ${props => props.isSoldOut ? 'var(--button-border-color)' : 'var(--main-color)'};
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  ${props => props.isSoldOut ? 'pointer-events: none;' : null};
 `;
 
-export default function FillButton({pageType}) {
+export default function FillButton({isSoldOut, pageType, text}) {
+
   return (
-    <Button pageType={pageType}>바로 구매</Button>
+    <Button pageType={pageType} isSoldOut={isSoldOut}>{text}</Button>
   )
 }

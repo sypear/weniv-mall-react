945c80be914c046f83b1bd30aab9ecbfb9c3f048
sypear <bsyeon015@gmail.com>
2022-11-29 22:42:00 +0900

    💄 CartButton 컴포넌트 background 스타일 추가

--- src/components/Buttons/CartButton/CartButton.jsx
+++ src/components/Buttons/CartButton/CartButton.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import CartIcon from '../../../assets/images/icon-shopping-cart.svg'
+import CartSoldOutIcon from '../../../assets/images/icon-shopping-cart-soldout.svg'
 
 const Button = styled.button`
   width: 60px;
   height: 60px;
   border: 1px solid var(--button-border-color);
   border-radius: 5px;
-  background: url(${CartIcon}) no-repeat center;
+  background: ${props => props.isSoldOut ? `var(--button-border-color) url(${CartSoldOutIcon})` : `url(${CartIcon})`} no-repeat center;
+  ${props => props.isSoldOut ? 'pointer-events: none;' : null};
 `;
 
-export default function CartButton() {
+export default function CartButton({isSoldOut}) {
   return (
-    <Button />
+    <Button isSoldOut={isSoldOut} />
   )
 }

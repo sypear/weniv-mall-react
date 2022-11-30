6bf9c9e55f39215a71084e38585a017cf5495976
sypear <bsyeon015@gmail.com>
2022-11-30 12:13:14 +0900

    🐛 모달창 뒷배경이 스크롤되는 현상 수정

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -46,6 +46,19 @@ const Modal = () => {
   const navigate = useNavigate();
   const backgroundRef = useRef(null);
 
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
+
   const handleCloseModal = (e) => {
     if (e.target === backgroundRef.current) {
       navigate(-1);
